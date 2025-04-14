bfd7876fc273010d4df06fd27809e40ef2cfc780
danasawalmeh <awaddana34@gmail.com>
2025-04-14 18:43:29 +0300

    Add files via upload

--- connect.php
+++ connect.php
@@ -4,8 +4,11 @@ $host="localhost";
 $user="root";
 $pass="";
 $db="menzabettha";
-$conn=new mysqli($host,$user,$pass,$db);
+$conn=mysqli_connect($host,$user,$pass,$db);
 if($conn->connect_error){
     echo "Failed to connect DB".$conn->connect_error;
+}
+else{
+    echo "you conected";
 }
 ?>
